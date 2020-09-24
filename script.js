2fdea7194414ef8a97450eac7bb2565a2cacab6c
Yogesh Sehgal <yogeshsehgal99@rediff.com>
2020-09-24 19:46:31 +0530

    Buttons Updates

---
+++ script.js
@@ -0,0 +1,55 @@
+const quizData = [
+  {
+    question: "National Rural Development Institute is situated at ?",
+    a: "Patna",
+    b: "Shimla",
+    c: "Hyderabad",
+    d: "Delhi",
+    answer: "c",
+  },
+  {
+    question: "National Income estimates in India is prepared by ?",
+    a: "RBI",
+    b: "Finance Ministry",
+    c: "C.S.O.",
+    d: "Planning Commission",
+    answer: "c",
+  },
+  {
+    question: "Dalal street is situated at ?",
+    a: "Delhi",
+    b: "Chennai",
+    c: "Kolkata",
+    d: "Mumbai",
+    answer: "d",
+  },
+  {
+    question: "The former name of State Bank of India was ?",
+    a: "United Bank of India",
+    b: "Imperial Bank of India",
+    c: "Britsh Bank of India",
+    d: "People's Bank of India",
+    answer: "b",
+  },
+  {
+    question:
+      "Foreign currency which has a tendency of quick migration is called ?",
+    a: "Hot Currency",
+    b: "Soft Currency",
+    c: "Scarce Currency",
+    d: "Gold Currency",
+    answer: "a",
+  },
+  {
+    question: "India earns maximum foreign exchange by the export of  ?",
+    a: "Steel",
+    b: "Textile",
+    c: "Iron",
+    d: "Tea",
+    answer: "d",
+  }
+];
+
+function loadQuiz(){
+    
+}
